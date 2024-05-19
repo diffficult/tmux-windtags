89b30aa2d41c8a7dba86764e75a4717c406f0936
diffficult <spaulo@gmail.com>
2024-05-19 17:22:25 -0300

    fix script output

--- scripts/window_tag.sh
+++ scripts/window_tag.sh
@@ -23,19 +23,14 @@ current_window_number=$(tmux display-message -p '#I')
 # Get the active window number using tmux list-windows command and parsing
 active_window_number=$(tmux list-windows | grep '(active)' | awk '{print $1}' | tr -d ':')
 
-# Debug output
-#echo "Current Window Number: $current_window_number"
-#echo "Active Window Number: $active_window_number"
-
 # Ensure both are integers before comparison
-if [ "$current_window_number" -eq "$current_window_number" ] 2>/dev/null && [ "$active_window_number" -eq "$active_window_number" ] 2>/dev/null; then
+if [[ "$current_window_number" =~ ^[0-9]+$ ]] && [[ "$active_window_number" =~ ^[0-9]+$ ]]; then
   # If the current window is active, set the active character, otherwise set the normal character
   if [ "$current_window_number" -eq "$active_window_number" ]; then
-    window_tag="${active_chars[$current_window_number]}"
+    echo "${active_chars[$current_window_number]}"
   else
-    window_tag="${normal_chars[$current_window_number]}"
+    echo "${normal_chars[$current_window_number]}"
   fi
-#  echo "$window_tag"  # This line is to output the variable for use in tmux.conf
 else
-  echo "  "
+  echo "Error: Window numbers are not valid integers."
 fi
